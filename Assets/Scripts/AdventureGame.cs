2f3df3e84db3f3cf1a3553c45ab8755485aad3ba
donrestarone <shashikejayatunge@gmail.com>
2019-03-10 17:01:38 -0400

    first commit

---
+++ Assets/Scripts/AdventureGame.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//to use Text as a data type you need to import this
+using UnityEngine.UI;
+
+public class AdventureGame : MonoBehaviour
+{
+    //by having serialize field, we have access to it in the inspector
+    //you can access text component as a reference
+    [SerializeField] Text textComponent;
+    [SerializeField] State StartingState;
+
+    State state;
+    // Start is called before the first frame update
+    void Start()
+    {
+        state = StartingState;
+        //take the textComponent and modify its text property.
+        textComponent.text = state.GetStateStory();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ManageState();
+    }
+
+    private void ManageState()
+    {
+        var nextStates = state.GetNextStates();
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            state = nextStates[0];
+
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            state = nextStates[1];
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            state = nextStates[2];
+        }
+
+        textComponent.text = state.GetStateStory();
+    }
+}
---
+++ Assets/Scripts/State.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(menuName ="State")]
+
+//this is a scriptable object. different from monobehavior which needs to be added as a component to a game object
+public class State : ScriptableObject
+{
+    //for adding a text box to the unity inspector
+    [TextArea(10,14)] [SerializeField] string storyText;
+
+    //create a serialized array of data type state
+    [SerializeField] State[] nextStates;
+
+    public string GetStateStory()
+    {
+        return storyText;
+    }
+
+    public State[] GetNextStates()
+    {
+        return nextStates; 
+    }
+}
