a765bef14254d9f79815a2543ab88a2799540956
donrestarone <shashikejayatunge@gmail.com>
2019-03-10 17:59:53 -0400

    debug index out of range bug when user presses an option that is beyond the limits set by state

--- Assets/Scripts/AdventureGame.cs
+++ Assets/Scripts/AdventureGame.cs
@@ -30,18 +30,15 @@ public class AdventureGame : MonoBehaviour
     private void ManageState()
     {
         var nextStates = state.GetNextStates();
-        if (Input.GetKeyDown(KeyCode.Alpha1))
-        {
-            state = nextStates[0];
 
-        }
-        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        for (int i = 0; i < nextStates.Length; i++)
         {
-            state = nextStates[1];
-        }
-        else if (Input.GetKeyDown(KeyCode.Alpha3))
-        {
-            state = nextStates[2];
+            if (Input.GetKeyDown(KeyCode.Alpha1 + i))
+            {
+                Debug.Log(i);
+                Debug.Log(nextStates.Length);
+                state = nextStates[i];
+            }
         }
 
         textComponent.text = state.GetStateStory();
